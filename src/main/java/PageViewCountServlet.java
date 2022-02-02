e590e335cd1b1f7f028417c4c4e64cc90961e9bc
James Wienecke <james.a.wienecke@gmail.com>
2022-02-02 11:55:30 -0600

    Began work on the page viewcount servlet.

---
+++ src/main/java/PageViewCountServlet.java
@@ -0,0 +1,12 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "PageViewCountServlet", urlPatterns = "/count")
+public class PageViewCountServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType();
+    }
+}
