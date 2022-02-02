c467b9fefc6cfdd0316605c7290458202ab18079
James Wienecke <james.a.wienecke@gmail.com>
2022-02-02 12:07:36 -0600

    Completed page viewcount servlet and its bonus goal.

--- src/main/java/PageViewCountServlet.java
+++ src/main/java/PageViewCountServlet.java
@@ -3,10 +3,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "PageViewCountServlet", urlPatterns = "/count")
 public class PageViewCountServlet extends HttpServlet {
+    private int viewCount = 0;
+
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType();
+        res.setContentType("text/html");
+        String reset = req.getQueryString();
+        PrintWriter out = res.getWriter();
+        if (reset == null) {
+            viewCount++;
+        } else {
+            viewCount = 0;
+        }
+        out.printf("<h1>Page has been viewed %d times", viewCount);
     }
 }
