d83ccd446c1d99c3d0cd08155446af20260257dd
James Wienecke <james.a.wienecke@gmail.com>
2022-02-07 09:33:11 -0600

    Filled out album javabean.

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -1,2 +1,49 @@
 public class Album {
+    private String artist;
+    private String name;
+    private int releaseDate;
+    private float sales;
+    private String genre;
+
+    public Album() { }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public float getSales() {
+        return sales;
+    }
+
+    public void setSales(float sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
