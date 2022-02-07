d518b89d6ee2c53ffcc4b1bc405c0aab5290525d
James Wienecke <james.a.wienecke@gmail.com>
2022-02-07 14:14:24 -0600

    Wrote the servlet to do the data retrieval and redirect the incoming request.

---
+++ src/main/java/ListAdsServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ListAdsServlet", urlPatterns = "/ads")
+public class ListAdsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Ads adsDao = DaoFactory.getAdsDao();
+        List<Ad> ads = adsDao.all();
+        req.setAttribute("ads", ads);
+        req.getRequestDispatcher("/ads/index.jsp").forward(req, res);
+    }
+}
