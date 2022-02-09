fccd6fcce2ab4d753b8100886dcb29c123ff5521
James Wienecke <james.a.wienecke@gmail.com>
2022-02-09 10:29:16 -0600

    Wrote out the Ads interface methods.

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,60 @@
+import java.sql.*;
+import com.mysql.cj.jdbc.Driver;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection;
+    public MySQLAdsDao() {
+        Config config = new Config();
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        List<Ad> ads = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM ads");
+            while (rs.next()) {
+                ads.add(new Ad(
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO ads (id, user_id, title, description) VALUES(" +
+                    ad.getId() + ", " +
+                    ad.getUserId() + ", " +
+                    ad.getTitle() + ", " +
+                    ad.getDescription() + ");", statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+            while (rs.next()) {
+                System.out.println("Ad creation success");
+            }
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+}
