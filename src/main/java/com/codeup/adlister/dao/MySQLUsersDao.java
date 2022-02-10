d40fe81222d544f38f0f199068bc36d59f185d7d
James Wienecke <james.a.wienecke@gmail.com>
2022-02-10 14:08:04 -0600

    Fixed issue with user dao insert method. Added session info grabbing for the ad creation process so the database gets all its nice foreign key business sorted

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -47,6 +47,8 @@ public class MySQLUsersDao implements Users {
             String sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
             PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
             stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
