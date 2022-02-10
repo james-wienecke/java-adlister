ebebc6f2550e25a09770277af1fcebcba716bef3
James Wienecke <james.a.wienecke@gmail.com>
2022-02-10 11:22:50 -0600

    Fixed Config package import and finished conversion of ads dao insert method to use prepared statements.

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -42,11 +42,11 @@ public class MySQLAdsDao implements Ads {
     public Long insert(Ad ad) {
         try {
             String sql = "INSERT INTO ads (user_id, title, description) VALUES (?, ?, ?)";
-            PreparedStatement stmt = connection.prepareStatement(sql);
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             stmt.setLong(1, ad.getUserId());
             stmt.setString(2, ad.getTitle());
             stmt.setString(3, ad.getDescription());
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+            stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
             return rs.getLong(1);
